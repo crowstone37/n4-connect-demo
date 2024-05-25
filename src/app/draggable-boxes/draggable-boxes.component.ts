dfb567721b4488df315d8ce9a74ed373f092f591
crowstone37 <seiten@awiworks.com>
2024-05-25 14:05:02 +0200

    added tailwind + css stuff + working version of drag box and connect them

--- src/app/draggable-boxes/draggable-boxes.component.ts
+++ src/app/draggable-boxes/draggable-boxes.component.ts
@@ -13,6 +13,9 @@ export class DraggableBoxesComponent implements AfterViewInit {
   @ViewChild('leftBox') leftBox!: ElementRef;
   @ViewChild('rightBox') rightBox!: ElementRef;
 
+
+  dragmode = 'queen';
+
   leftBoxPosition = { x: 50, y: 200 };
   rightBoxPosition = { x: 500, y: 200 };
   pathData: string = '';
@@ -21,11 +24,29 @@ export class DraggableBoxesComponent implements AfterViewInit {
   mouseX: number = 0;
   mouseY: number = 0;
 
+  constructor(private cdr: ChangeDetectorRef){
+
+  }
+
   ngAfterViewInit(): void {
     this.updatePath();
   }
 
+  moveContainer(){
+    this.draggingEnd = 'left'
+    this.dragmode = 'queen'
+    this.updatePath();
+  }
+
+  moveContainerEnd(){
+    this.dragmode = 'queen'
+    this.updatePath();
+    this.dragmode = 'king'
+
+  }
+
   onMouseDown(box: 'left' | 'right', event: MouseEvent): void {
+    this.dragmode = 'king';
     this.draggingEnd = box;
     this.mouseX = event.clientX;
     this.mouseY = event.clientY;
@@ -37,19 +58,22 @@ export class DraggableBoxesComponent implements AfterViewInit {
     if (this.draggingEnd !== 'none') {
       this.mouseX = event.clientX;
       this.mouseY = event.clientY;
-      this.updatePath();
     }
+    this.updatePath();
   }
 
   @HostListener('document:mouseup', ['$event'])
   onMouseUp(event: MouseEvent): void {
+    this.dragmode = 'queen';
     if (this.draggingEnd !== 'none') {
       this.mouseX = event.clientX;
       this.mouseY = event.clientY;
       
       this.updatePath();
       this.draggingEnd = 'none';
+      this.dragmode = 'king';
     }
+    this.updatePath();
   }
 
   private updatePath(): void {
@@ -62,10 +86,11 @@ export class DraggableBoxesComponent implements AfterViewInit {
       let endX = rightBoxRect.left;
       let endY = rightBoxRect.top + rightBoxRect.height / 2;
 
-      if (this.draggingEnd === 'left') {
+      if (this.draggingEnd === 'left' && this.dragmode == 'king') {
         endX = this.mouseX;
         endY = this.mouseY;
-      } else if (this.draggingEnd === 'right') {
+      }    
+      else if (this.draggingEnd === 'right') {
         startX = this.mouseX;
         startY = this.mouseY;
       }
@@ -75,7 +100,12 @@ export class DraggableBoxesComponent implements AfterViewInit {
       const controlX2 = startX + (endX - startX) / 2;
       const controlY2 = endY;
 
+      // if (this.draggingEnd === 'left' && this.dragmode == 'queen') {
+      //   console.log(`M ${startX} ${startY} C ${controlX1} ${controlY1}, ${controlX2} ${controlY2}, ${endX} ${endY}`)
+      // }  
+
       this.pathData = `M ${startX} ${startY} C ${controlX1} ${controlY1}, ${controlX2} ${controlY2}, ${endX} ${endY}`;
+      this.cdr.detectChanges();
     }
   }
 }
